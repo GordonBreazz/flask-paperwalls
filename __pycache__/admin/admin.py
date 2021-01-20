58c51a83ecf02cb48b068d9aa83da6a7930870e5
GordonBreazz <jc12-mail@yandex.ru>
2021-01-20 23:52:32 +0800

    fix

--- __pycache__/admin/admin.py
+++ __pycache__/admin/admin.py
@@ -8,6 +8,22 @@ class MyView(BaseView):
 
 app = Flask(__name__)
 
+def post(self, id):
+      parser = reqparse.RequestParser()
+      parser.add_argument("author")
+      parser.add_argument("quote")
+      params = parser.parse_args()
+      for quote in ai_quotes:
+          if(id == quote["id"]):
+              return f"Quote with id {id} already exists", 400
+      quote = {
+          "id": int(id),
+          "author": params["author"],
+          "quote": params["quote"]
+      }
+      ai_quotes.append(quote)
+      return quote, 201
+
 admin = Admin(app)
 admin.add_view(MyView(name='Hello 1', endpoint='test1', category='Test'))
 admin.add_view(MyView(name='Hello 2', endpoint='test2', category='Test'))
